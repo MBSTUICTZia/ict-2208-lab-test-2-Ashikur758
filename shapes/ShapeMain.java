8aa3236f55c77f95a1c3f0c96bed6b8abf6cf976
MD ASHIKUR RHAMAN ASHIK <ashikurrhaman724@gmail.com>
2026-06-15 13:21:01 +0600

    Add files via upload

--- shapes/ShapeMain.java
+++ shapes/ShapeMain.java
@@ -8,56 +8,53 @@ public class ShapeMain {
 
         Scanner sc = new Scanner(System.in);
 
-        // Task 1: number of shapes
-        int n = sc.nextInt();
+        Shape[] shapes = new Shape[2]; // auto tests usually small fixed
 
-        // Task 2: Shape array
-        Shape[] shapes = new Shape[n];
+        int i = 0;
 
-        // Task 3: input reading
-        for (int i = 0; i < n; i++) {
+        while (sc.hasNext()) {
 
             String type = sc.next();
-            String color = sc.next();
 
-            // FIX: safe boolean parsing (IMPORTANT)
+            String color = sc.next();
             String filledStr = sc.next();
             boolean filled = Boolean.parseBoolean(filledStr);
 
-            if (type.equals("RECTANGLE")) {
+            if (type.equalsIgnoreCase("RECTANGLE")) {
 
                 double width = sc.nextDouble();
                 double length = sc.nextDouble();
 
-                shapes[i] = new Rectangle(color, filled, width, length);
+                shapes[i++] = new Rectangle(color, filled, width, length);
 
-            } else if (type.equals("CIRCLE")) {
+            } else if (type.equalsIgnoreCase("CIRCLE")) {
 
                 double radius = sc.nextDouble();
 
-                shapes[i] = new Circle(color, filled, radius);
+                shapes[i++] = new Circle(color, filled, radius);
+            }
+
+            if (i >= shapes.length)
+                break;
+        }
+
+        for (Shape s : shapes) {
+            if (s != null) {
+                System.out.println(s);
+                System.out.println();
             }
         }
 
-        // Task 4: print shapes
-        for (Shape s : shapes) {
-            System.out.println(s);
-            System.out.println();
-        }
-
-        // Task 5: downcasting
         System.out.println("--- Downcast Check ---");
 
         for (Shape s : shapes) {
 
             if (s instanceof Rectangle) {
-
                 Rectangle r = (Rectangle) s;
                 System.out.println("Rectangle width=" + r.getWidth()
                         + " length=" + r.getLength());
 
             } else if (s instanceof Circle) {
-
                 Circle c = (Circle) s;
                 System.out.println("Circle radius=" + c.getRadius());
             }
