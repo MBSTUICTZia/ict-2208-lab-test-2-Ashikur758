14505d77fbe8e44c19024b0a30637ebecb817e43
MD ASHIKUR RHAMAN ASHIK <ashikurrhaman724@gmail.com>
2026-06-15 13:19:25 +0600

    Add files via upload

--- shapes/ShapeMain.java
+++ shapes/ShapeMain.java
@@ -8,32 +8,59 @@ public class ShapeMain {
 
         Scanner sc = new Scanner(System.in);
 
-        while (sc.hasNext()) {
+        // Task 1: number of shapes
+        int n = sc.nextInt();
 
-            String type = sc.next().toLowerCase();
+        // Task 2: Shape array
+        Shape[] shapes = new Shape[n];
+
+        // Task 3: input reading
+        for (int i = 0; i < n; i++) {
+
+            String type = sc.next();
             String color = sc.next();
-            boolean filled = sc.nextBoolean();
 
-            Shape shape;
+            // FIX: safe boolean parsing (IMPORTANT)
+            String filledStr = sc.next();
+            boolean filled = Boolean.parseBoolean(filledStr);
 
-            if (type.equals("rectangle")) {
+            if (type.equals("RECTANGLE")) {
 
                 double width = sc.nextDouble();
                 double length = sc.nextDouble();
 
-                shape = new Rectangle(color, filled, width, length);
+                shapes[i] = new Rectangle(color, filled, width, length);
 
-            } else {
+            } else if (type.equals("CIRCLE")) {
 
                 double radius = sc.nextDouble();
 
-                shape = new Circle(color, filled, radius);
+                shapes[i] = new Circle(color, filled, radius);
             }
+        }
 
-            System.out.println(shape.toString());
-            System.out.printf("Area: %.2f%n", shape.getArea());
-            System.out.printf("Perimeter: %.2f%n", shape.getPerimeter());
+        // Task 4: print shapes
+        for (Shape s : shapes) {
+            System.out.println(s);
             System.out.println();
+        }
+
+        // Task 5: downcasting
+        System.out.println("--- Downcast Check ---");
+
+        for (Shape s : shapes) {
+
+            if (s instanceof Rectangle) {
+
+                Rectangle r = (Rectangle) s;
+                System.out.println("Rectangle width=" + r.getWidth()
+                        + " length=" + r.getLength());
+
+            } else if (s instanceof Circle) {
+
+                Circle c = (Circle) s;
+                System.out.println("Circle radius=" + c.getRadius());
+            }
         }
 
         sc.close();
